6cafa3d4784b6ddac1c27d0f69019058af9fd4ad
Jakob <43860560+jakbellamy@users.noreply.github.com>
2020-10-15 18:46:15 -0400

    cleaned

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import pandas as pd
 from Genders_Binary import Girl, Boy
 from random import randint as ran
 
+
 #   Input Dialogues
 def bad_in():
     print('[-] Bad Input.\n[-] ')
@@ -27,9 +28,11 @@ data = []
 count = 0
 while count < iterative:
     if gender_choice == '1':
-        person = Girl(age=ran(14, 70), height=ran(140, 220), haircolor=ran(0, 4), hairlength=ran(0, 2), boobsize=ran(0, 3), figure=ran(0, 4), eyecolor=ran(0, 3))
+        person = Girl(age=ran(14, 70), height=ran(140, 220), haircolor=ran(0, 4), hairlength=ran(0, 2),
+                      boobsize=ran(0, 3), figure=ran(0, 4), eyecolor=ran(0, 3))
     else:
-        person = Boy(age=ran(14, 70), height=ran(140, 220), haircolor=ran(0, 4), hairlength=ran(0, 2), eyecolor=ran(0, 3), beard=ran(0, 3), body=ran(0, 2))
+        person = Boy(age=ran(14, 70), height=ran(140, 220), haircolor=ran(0, 4), hairlength=ran(0, 2),
+                     eyecolor=ran(0, 3), beard=ran(0, 3), body=ran(0, 2))
     data += [person.__dict__]
     count += 1
 
